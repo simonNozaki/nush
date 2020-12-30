83c1a5f9b325b99dfe9c014904a18c49f51a1882
snozaki <snozaki@netprotections.co.jp>
2020-12-30 20:54:03 +0900

    switchを更新

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-// stripe関連
 const env = require("dotenv").config({ path: "./.env" });
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-// logger
 const logger = require("../logger");
 
 /* GET home page. */
@@ -12,9 +10,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-/**
- * 決済を確定する
- */
 router.post("/v1/order/payment", async function(req, res, next){
 
   logger.info("ルータメソッドの処理を開始します. リクエスト : ", req.body);
@@ -59,7 +54,7 @@ router.post("/v1/order/payment", async function(req, res, next){
 
 /**
  * 商品総額の計算
- * @param items 
+ * @param items 商品オブジェクト配列
  */
 function calculateAmount(items) {
   let total = 0;
@@ -73,49 +68,50 @@ function calculateAmount(items) {
 
 /**
  * Controllerのレスポンスをステータスにあわせて生成する
- * @param paymentIntent 
+ * @param paymentIntent PaymentIntent
  */
 function generateResponse(paymentIntent) {
-  // レスポンスの初期化
   let response = {
     requiresAction: false,
     clientSecret: "",
     paymentIntentStatus : ""
   }
 
-  // ステータスに応じてレスポンスを設定
   switch (paymentIntent.status) {
     case "requires_action":
-      response.paymentIntentStatus = "requires_action";
-      response.clientSecret = paymentIntent.client_secret;
-      break;
+        response.paymentIntentStatus = "requires_action";
+        break;
     case "requires_source_action":
-      response.paymentIntentStatus = "requires_source_action";
-      response.requiresAction = true;
-      response.clientSecret = paymentIntent.client_secret;
-      break;
+        response.paymentIntentStatus = "requires_source_action";
+        response.requiresAction = true;
+        response.clientSecret = paymentIntent.client_secret;
+        break;
     case "requires_payment_method":
-      response.paymentIntentStatus = "requires_payment_method";
-      break;
+        response.paymentIntentStatus = "requires_payment_method";
+        break;
     case "requires_source":
-      response.paymentIntentStatus = "requires_source";
-
-      response.error = {
-        messages : ["カードが拒否されました。別の決済手段をお試しください"]
-      }
-      break;
+        response.paymentIntentStatus = "requires_source";
+        response.error = {
+            messages : ["カードが拒否されました。別の決済手段をお試しください"]
+        }
+        break;
     case "succeeded":
-      response.paymentIntentStatus = "succeeded";
-      response.clientSecret = paymentIntent.client_secret;
-      break;
-  }
+        response.paymentIntentStatus = "succeeded";
+        response.clientSecret = paymentIntent.client_secret;
+        break;
+    default:
+        response.error = {
+            messages : ["システムエラーが発生しました"]
+        }
+        break;
+    }
 
   return response;
 }
 
 /**
  * エラーレスポンスを生成する
- * @param {*} error 
+ * @param error エラーメッセージ文字列
  */
 function generateErrorResponse (error) {
 
